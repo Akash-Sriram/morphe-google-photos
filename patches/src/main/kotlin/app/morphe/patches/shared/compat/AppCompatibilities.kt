f05037e17f1c0b57fdf1a08f51855a0afcb3a982
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2026-09-15 13:43:16 +0200

    fix(Pixiv): Update compatibility declaration

--- patches/src/main/kotlin/app/morphe/patches/shared/compat/AppCompatibilities.kt
+++ patches/src/main/kotlin/app/morphe/patches/shared/compat/AppCompatibilities.kt
@@ -124,16 +124,16 @@ internal object AppCompatibilities {
         appIconColor = 0x00B020,
     )
 
-    val PIXIV_ADS = Compatibility(
+    val PIXIV = Compatibility(
         name = "Pixiv",
         packageName = "jp.pxv.android",
         appIconColor = 0x0096FA,
         targets = listOf(
-            AppTarget("6.196.0"),
+            AppTarget("6.141.1"),
         )
     )
 
-    val PIXIV = PIXIV_ADS.including(AppTarget("6.141.1"))
+    val PIXIV_ADS = PIXIV.including(AppTarget("6.196.0"))
 
     val CRICBUZZ = Compatibility(
         name = "Cricbuzz",
